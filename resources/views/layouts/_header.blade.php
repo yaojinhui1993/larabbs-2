14d87f4d45296c31c9420beafcfe23dce50d5845
yaojinhui <yaojinhui1993@gmail.com>
2020-02-24 13:01:35 +0800

    feat: user auth skelton
    1. auth:ui
    2. translator

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -13,10 +13,10 @@
 
             <ul class="navbar-nav navbar-right">
                 <li class="nav-item">
-                    <a href="#" class="nav-link">登录</a>
+                    <a href="{{ route('login') }}" class="nav-link">登录</a>
                 </li>
                 <li class="nav-item">
-                    <a href="#" class="nav-link">注册</a>
+                    <a href="{{ route('register') }}" class="nav-link">注册</a>
                 </li>
             </ul>
         </div>
