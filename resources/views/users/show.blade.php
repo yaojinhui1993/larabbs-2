a33b5dfe02d531ce508daafea76a7dff11959566
yaojinhui <yaojinhui1993@gmail.com>
2020-02-26 14:21:40 +0800

    feat: show user page.

---
+++ resources/views/users/show.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.app')
+
+@section('title', $user->name . ' 的个人中心')
+
+@section('content')
+    <div class="row">
+        <div class="col-lg-3 col-md-3 hidden-sm hidden-xs user-info">
+            <div class="card">
+                <img src="https://cdn.learnku.com/uploads/images/201709/20/1/PtDKbASVcz.png?imageView2/1/w/600/h/600" alt="{{ $user->name }}" class="card-img-top">
+                <div class="card-body">
+                    <h5>
+                        <strong>个人简介</strong>
+                    </h5>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum nihil iure optio velit suscipit amet eligendi accusantium eveniet quia deleniti perspiciatis magni reprehenderit voluptatibus nisi tenetur ratione, vero repellendus magnam!</p>
+                    <hr>
+                    <h5>
+                        <strong>注册于</strong>
+                    </h5>
+                    <p>January 01 1901</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-lg-9 col-md-9 col-sm-12 col-xs-12">
+            <div class="card">
+                <div class="card-body">
+                    <h1 class="mb-0" style="font-size: 22px;">
+                        {{ $user->name }} <small>{{ $user->email }}</small>
+                    </h1>
+                </div>
+            </div>
+
+            <hr>
+
+            <div class="card">
+                <div class="card-body">
+                    暂无数据 ~_~
+                </div>
+            </div>
+        </div>
+
+    </div>
+@endsection
