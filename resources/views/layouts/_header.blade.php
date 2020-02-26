0c5344e355a4382372ab7a04c8d7c19e54e6efd8
yaojinhui <yaojinhui1993@gmail.com>
2020-02-26 20:02:28 +0800

    feat: edit personal data
    1. User form request
    2. Alter table
    3. Show form errors

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -25,9 +25,10 @@
                             <img src="https://cdn.learnku.com/uploads/images/201709/20/1/PtDKbASVcz.png?imageView2/1/w/60/h/60" class="img-responsive img-circle" width="30px" height="30px">
                             {{ Auth::user()->name }}
                         </a>
+
                         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a href="#" class="dropdown-item">个人中心</a>
-                            <a href="#" class="dropdown-item">编辑资料</a>
+                            <a href="{{ route('users.show', Auth::id()) }}" class="dropdown-item">个人中心</a>
+                            <a href="{{ route('users.edit', Auth::id()) }}" class="dropdown-item">编辑资料</a>
                             <div class="dropdown-divider"></div>
                             <a href="#" class="dropdown-item" id="logout">
                                 <form action="{{ route('logout') }}" method="POST">
