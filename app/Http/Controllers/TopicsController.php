cdba3a16ebfce158e0fc351741481b0c5a3d0b8e
yaojinhui <yaojinhui1993@gmail.com>
2020-03-04 08:54:48 +0800

    feat: user can create topics
    1. make excerpt
    2. add create topic router
    3. create topic
    4. use observer
    5. use form request

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -3,9 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\Topic;
+use App\Models\Category;
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
 use App\Http\Requests\TopicRequest;
+use Illuminate\Support\Facades\Auth;
 
 class TopicsController extends Controller
 {
@@ -14,50 +15,55 @@ class TopicsController extends Controller
         $this->middleware('auth', ['except' => ['index', 'show']]);
     }
 
-	public function index(Request $request)
-	{
+    public function index(Request $request)
+    {
         $topics = Topic::
             with(['category', 'user'])
             ->withOrder($request->order)
             ->paginate();
-		return view('topics.index', compact('topics'));
-	}
+        return view('topics.index', compact('topics'));
+    }
 
     public function show(Topic $topic)
     {
         return view('topics.show', compact('topic'));
     }
 
-	public function create(Topic $topic)
-	{
-		return view('topics.create_and_edit', compact('topic'));
-	}
+    public function create(Topic $topic)
+    {
+        $categories = Category::all();
 
-	public function store(TopicRequest $request)
-	{
-		$topic = Topic::create($request->all());
-		return redirect()->route('topics.show', $topic->id)->with('message', 'Created successfully.');
-	}
+        return view('topics.create_and_edit', compact('topic', 'categories'));
+    }
 
-	public function edit(Topic $topic)
-	{
+    public function store(TopicRequest $request)
+    {
+        $topic = Topic::make($request->all());
+        $topic->user_id = Auth::id();
+        $topic->save();
+
+        return redirect()->route('topics.show', $topic->id)->with('success', '帖子创建成功！');
+    }
+
+    public function edit(Topic $topic)
+    {
         $this->authorize('update', $topic);
-		return view('topics.create_and_edit', compact('topic'));
-	}
+        return view('topics.create_and_edit', compact('topic'));
+    }
 
-	public function update(TopicRequest $request, Topic $topic)
-	{
-		$this->authorize('update', $topic);
-		$topic->update($request->all());
+    public function update(TopicRequest $request, Topic $topic)
+    {
+        $this->authorize('update', $topic);
+        $topic->update($request->all());
 
-		return redirect()->route('topics.show', $topic->id)->with('message', 'Updated successfully.');
-	}
+        return redirect()->route('topics.show', $topic->id)->with('message', 'Updated successfully.');
+    }
 
-	public function destroy(Topic $topic)
-	{
-		$this->authorize('destroy', $topic);
-		$topic->delete();
+    public function destroy(Topic $topic)
+    {
+        $this->authorize('destroy', $topic);
+        $topic->delete();
 
-		return redirect()->route('topics.index')->with('message', 'Deleted successfully.');
-	}
+        return redirect()->route('topics.index')->with('message', 'Deleted successfully.');
+    }
 }
--- app/helpers.php
+++ app/helpers.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Route;
 
 function route_class()
@@ -10,4 +11,11 @@ function route_class()
 function category_nav_active($categoryId)
 {
     return active_class(if_route('categories.show') && if_route_param('category', $categoryId));
+}
+
+function make_excerpt($string, $length = 200)
+{
+    $excerpt = trim(preg_replace('/\r\n|\r|\n+/', ' ', strip_tags($string)));
+
+    return Str::limit($excerpt, $length);
 }
