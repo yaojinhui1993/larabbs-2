f7ace3a799d1128ba1cd8c34d7592add0c61990c
yaojinhui <yaojinhui1993@gmail.com>
2020-03-05 12:32:04 +0800

    feat: topic content show page.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="description" content="@yield('description', 'LaraBBS')">
     <title>@yield('title', 'LaraBBS')</title>
 
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
