b8716c01ce5477e0b8c97b236605af1fc6e80c12
yaojinhui <yaojinhui1993@gmail.com>
2020-03-11 09:18:16 +0800

    feat: replies list

--- resources/views/topics/show.blade.php
+++ resources/views/topics/show.blade.php
@@ -56,7 +56,15 @@
                     </div>
                 @endcan
             </div>
+
+            <div class="card topic-reply mt-4">
+                <div class="card-body">
+                    @include('topics._reply_box', ['topic' => $topic])
+                    @include('topics._reply_list', ['replies' => $topic->replies()->with('user')->get()])
+                </div>
+            </div>
         </div>
+
 
     </div>
 
